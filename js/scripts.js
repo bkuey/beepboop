555c9c4324baaa496a3af7ad91ed47891fa0c90f
Mai Kue Lor <kuey27@gmail.com>
2019-11-01 16:41:34 -0700

    make changes to js file to find another way to make the application work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
   $(".mainForm").submit(function(event) {
     var input = parseInt($("#input").val());
-    var result = beepBoop(input);
-    $("#result").text(result).show();
+    $("#result").append("<p>[" + beepBoop(input) + "]<p>");
 
   event.preventDefault();
   });
 });
 
-function beepBoop(number) {
-  var result = "";
-
-    if (number === 1) {
-      result += "Beep";
-    } else if (number === 2) {
-      result += "Boop";
-    } else if (number === 3) {
-      result += "I'm sorry, Dave. I'm afraid I can't do that."
-    }
-
-  return result;
-}
+function beepBoop(input) {
+  var array = [];
+  var beep = "Beep!";
+  var boop = "Boop!";
+  var sowwy = "I'm sorry Dave" + "I'm afraid I can't do that.";
+    for (var i = 0; i <= input; i++) {
+      userString = input.toString();
+      if (i % 3 === 0 && i > 0) {
+        array.push(sowwy);
+      } else if (userString.includes("1")) {
+        array.push(boop);
+      } else if (userString.includes("0")) {
+        array.push(beep);
+      } else {
+        array.push(i.toString());
+        }
+      }
+      return array;
+  }
